af7f08ea50b3bcb59dad44e15cc802243cf95f50
abelb01 <44084096+abelb01@users.noreply.github.com>
2022-01-10 22:33:08 +0000

    added helm chart

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ LABEL version="Velociraptor v0.6.1"
 LABEL description="Velociraptor server in a Docker container"
 LABEL maintainer="Wes Lambert, @therealwlambert"
 ENV VERSION="0.6.1"
-RUN apt-get update && \
+COPY ./entry.sh .
+RUN chmod +x entry.sh && \
+    apt-get update && \
     apt-get install -y curl wget jq rsync && \
     # Create dirs for Velo binaries
     mkdir -p /opt/velociraptor && \
@@ -27,7 +29,7 @@ WORKDIR /velociraptor
 RUN cp /opt/velociraptor/linux/velociraptor . && chmod +x velociraptor && \
     mkdir -p /velociraptor/clients/linux && rsync -a /opt/velociraptor/linux/velociraptor /velociraptor/clients/linux/velociraptor_client &&\
     mkdir /velociraptor/startup
-#    chmod +x /velociraptor/startup/entry.sh
+    chmod +x /velociraptor/startup/entry.sh
 
 # Configmap details
-CMD ['/velociraptor/startup/entry.sh']
+CMD ['entry.sh']
---
+++ entry.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+CLIENT_DIR="/velociraptor/clients"
+cp /opt/velociraptor/linux/velociraptor . && chmod +x velociraptor
+mkdir -p $CLIENT_DIR/linux && rsync -a /opt/velociraptor/linux/velociraptor /velociraptor/clients/linux/velociraptor_client
+mkdir -p $CLIENT_DIR/mac && rsync -a /opt/velociraptor/mac/velociraptor_client /velociraptor/clients/mac/velociraptor_client
+mkdir -p $CLIENT_DIR/windows && rsync -a /opt/velociraptor/windows/velociraptor_client* /velociraptor/clients/windows/
+echo "mkdir done" && \
+echo '{"Frontend":{"public_path":"'$PUBLIC_PATH'", "hostname":"'$VELOX_FRONTEND_HOSTNAME'"}, "API":{"bind_address":"'$BIND_ADDRESS'"}, "GUI":{"bind_address":"'$BIND_ADDRESS'"}, "Monitoring":{"bind_address":"'$BIND_ADDRESS'"}, "Logging":{"output_directory":"'$LOG_DIR'", "separate_logs_per_component":"'$LOG_PER_COMPONENT'"}, "Client":{"server_urls":["'$VELOX_SERVER_URL'"], "use_self_signed_ssl":"'$SELF_SIGNED_CERT'"}, "Datastore":{"location":"'$DATASTORE_LOCATION'", "filestore_directory":"'$FILESTORE_DIRECTORY'"}}' && \
+./velociraptor config generate > server.config.yaml --merge '{"Frontend":{"public_path":"'$PUBLIC_PATH'", "hostname":"'$VELOX_FRONTEND_HOSTNAME'"}, "API":{"bind_address":"'$BIND_ADDRESS'"}, "GUI":{"bind_address":"'$BIND_ADDRESS'"}, "Monitoring":{"bind_address":"'$BIND_ADDRESS'"}, "Logging":{"output_directory":"'$LOG_DIR'", "separate_logs_per_component":"'$LOG_PER_COMPONENT'"}, "Client":{"server_urls":["'$VELOX_SERVER_URL'"], "use_self_signed_ssl":"'$SELF_SIGNED_CERT'"}, "Datastore":{"location":"'$DATASTORE_LOCATION'", "filestore_directory":"'$FILESTORE_DIRECTORY'"}}' &&\
+echo "merge done" && \
+./velociraptor --config server.config.yaml user add $VELOX_USER $VELOX_PASSWORD --role administrator &&\
+echo "added user" && \
+./velociraptor --config server.config.yaml config client > client.config.yaml &&\
+echo "client config done" && \
+./velociraptor config repack --exe clients/linux/velociraptor_client client.config.yaml clients/linux/velociraptor_client_repacked &&\
+./velociraptor config repack --exe clients/mac/velociraptor_client client.config.yaml clients/mac/velociraptor_client_repacked &&\
+./velociraptor config repack --exe clients/windows/velociraptor_client.exe client.config.yaml clients/windows/velociraptor_client_repacked.exe &&\
+echo "clients repacked" && \
+./velociraptor --config server.config.yaml frontend -v
